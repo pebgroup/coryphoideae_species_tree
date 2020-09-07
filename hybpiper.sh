477d837718a0ba43d33a4615ae6822dcc9c352b7
Mads Peter Petø peter.petoe@bio.au.dk <petoe@fe1.genomedk.net>
2020-09-07 13:16:03 +0200

    test

--- hybpiper.sh
+++ hybpiper.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# test
+
 while read name
 do
 ~/github/HybPiper/reads_first.py --cpu 16 -r ../2_trimmed/"$name"_1P.fastq ../2_trimmed/"$name"_2P.fastq --unpaired ../2_trimmed/"$name"_UN.fastq -b /home/petoe/faststorage/target_directory/PhyloPalms_loci_renamed_794-176_HEYcorrected.fasta --prefix $name --bwa
