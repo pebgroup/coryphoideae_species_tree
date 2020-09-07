7a591d61d24bf8f09572ad0c7bb562ea8d582869
Peter <peter.petoe@bios.au.dk>
2020-09-07 13:11:08 +0200

    safekeeping

--- hybpiper.sh
+++ hybpiper.sh
@@ -2,6 +2,6 @@
 
 while read name
 do
-~/github/HybPiper/reads_first.py --cpu 16 -r ../2_trimmed/"$name"_1P.fastq ../2_trimmed/"$name"_2P.fastq --unpaired ../2_trimmed/"$name"_UN.fastq -b /data_vol/peter/target/PhyloPalms_loci_renamed_794-176_HEYcorrected.fasta --prefix $name --bwa
+~/github/HybPiper/reads_first.py --cpu 16 -r ../2_trimmed/"$name"_1P.fastq ../2_trimmed/"$name"_2P.fastq --unpaired ../2_trimmed/"$name"_UN.fastq -b /home/petoe/faststorage/target_directory/PhyloPalms_loci_renamed_794-176_HEYcorrected.fasta --prefix $name --bwa
 done < namelist.txt
 
