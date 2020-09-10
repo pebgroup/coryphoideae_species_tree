cd1aa8577d9d19b424e54d8f420dd51857944e17
Peter <peter.petoe@bios.au.dk>
2020-09-10 11:39:30 +0200

    update

--- hybpiper.sh
+++ hybpiper.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-#SBATCH --partition normal
-#SBATCH --mem-per-cpu 4G
-#SBATCH -c 16
 
 while read name
 do
-~/github/HybPiper/reads_first.py --cpu 16 -r ../2_trimmed/"$name"_1P.fastq ../2_trimmed/"$name"_2P.fastq --unpaired ../2_trimmed/"$name"_UN.fastq -b /home/petoe/faststorage/target_directory/PhyloPalms_loci_renamed_794-176_HEYcorrected.fasta --prefix $name --bwa
+~/github/hybpiper/reads_first.py --cpu 7 -r ../2_trimmed/"$name"_1P.fastq ../2_trimmed/"$name"_2P.fastq --unpaired ../2_trimmed/"$name"_UN.fastq -b /home/au297565/Documents/temp/sidonie_target_file/PhyloPalms_loci_renamed_794-176_HEYcorrected.fasta --prefix $name --bwa
 done < namelist.txt
 
