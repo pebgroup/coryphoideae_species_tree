422af032a79f51d0b82dea4f05d40737e9b74c13
Mads Peter Petø peter.petoe@bio.au.dk <petoe@fe1.genomedk.net>
2020-09-11 15:40:22 +0200

    update

--- mafft.sh
+++ mafft.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+#SBATCH --partition normal
+#SBATCH --mem-per-cpu 4G
+#SBATCH --cpus-per-task 64
+#SBATCH --time 12:00:00
+
 ## This script runs mafft on the fasta files contained within the directory from which it is executed. Remember to adjust number of threads. 
 ## From the manual: "*L-INS-i (probably most accurate; recommended for <200 sequences; iterative refinement method incorporating local pairwise alignment information): mafft --localpair --maxiterate 1000 input [> output] linsi input [> output]". These settings are recommended by Matt Johnson in the KewHybSeqWorkshop.
 
 for f in *; do 
-	linsi --thread 16 $f > ../5_alignments/${f/.FNA}_aligned.fasta
+	linsi --thread 64 $f > ../5_alignments/${f/.FNA}_aligned.fasta
 done
 
 ## Wolf's repo:
