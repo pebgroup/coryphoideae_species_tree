0443945914d37f0449d2d49c9c617e760beb9b0c
Oscar <owkien@gmail.com>
2021-04-09 12:25:09 +0200

    Secapr_batch

---
+++ Slurm-scripts/secapr_batch.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH --account Coryphoideae
+#SBATCH --job-name=Secapr
+#SBATCH --partition normal
+#SBATCH --mem-per-cpu=20g
+#SBATCH --cpus-per-task=16
+
+#This line should enable the activation of specific conda environments
+source /home/owrisberg/miniconda3/etc/profile.d/conda.sh
+
+#Activating conda base environment 
+conda activate secapr_env
+
+#Going to folder with data
+cd /home/owrisberg/Coryphoideae/work_flow/1_data
+
+# Running Intronerate
+secapr quality_check --input . --output .
+
