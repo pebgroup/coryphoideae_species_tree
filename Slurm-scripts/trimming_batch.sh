a519b9b939c0753a6cdf5c935a9b10fe3f2e2ef4
Oscar <owkien@gmail.com>
2021-04-09 08:41:17 +0200

    Updated name lists and added intronerate and trimmomatic batch scripts

---
+++ Slurm-scripts/trimming_batch.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#SBATCH --job-name=Trimming
+#SBATCH --account Coryphoideae
+#SBATCH --partition normal
+#SBATCH --mem-per-cpu 8G
+#SBATCH -c 1
+
+
+#This line should enable the activation of specific conda environments
+source /home/owrisberg/miniconda3/etc/profile.d/conda.sh
+
+#Activating conda trimmomatic environment 
+conda activate trimmomatic_env
+
+#Going to folder with data
+cd /home/owrisberg/Coryphoideae/work_flow/2_trimmed
+
+# Running the trimmomatic
+bash /home/owrisberg/Coryphoideae/github_code/coryphoideae_species_tree/trimmomatic_genomedk_sh.sh
+
