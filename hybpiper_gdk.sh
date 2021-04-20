9d81f198517349c56245b4e76e1f8efd23d3fa43
Oscar <owkien@gmail.com>
2021-04-20 22:01:30 +0200

    edited hybpiper_gdk to work with new file structure

--- hybpiper_gdk.sh
+++ hybpiper_gdk.sh
@@ -2,6 +2,6 @@
 
 while read name
 do
-/home/owrisberg/Coryphoideae/github_code/HybPiper/reads_first.py --cpu 10 --readfiles ../2_trimmed/"$name"_1P.fastq ../2_trimmed/"$name"_2P.fastq --unpaired ../2_trimmed/"$name"_UN.fastq -b /home/owrisberg/Coryphoideae/target_sequence/PhyloPalms_loci_renamed_794-176_HEYcorrected.fasta --prefix $name --bwa
+/home/owrisberg/Coryphoideae/github_code/HybPiper/reads_first.py --cpu 10 --readfiles ../02_trimmed/"$name"_1P.fastq ../02_trimmed/"$name"_2P.fastq --unpaired ../02_trimmed/"$name"_UN.fastq -b /home/owrisberg/Coryphoideae/target_sequence/PhyloPalms_loci_renamed_794-176_HEYcorrected.fasta --prefix $name --bwa
 done < namelist.txt
 
