861f4fe421bf185bf87780677ec531b6d5d961df
Oscar <owkien@gmail.com>
2021-04-26 10:00:59 +0200

    updated file-path for intronerate and added time.

--- Slurm-scripts/intronerate_batch.sh
+++ Slurm-scripts/intronerate_batch.sh
@@ -5,6 +5,8 @@
 #SBATCH --partition normal
 #SBATCH --mem-per-cpu=20g
 #SBATCH --cpus-per-task=24
+#              D-HH:MM:SS
+#SBATCH --time=1-00:00:00
 
 #This line should enable the activation of specific conda environments
 source /home/owrisberg/miniconda3/etc/profile.d/conda.sh
@@ -13,7 +15,7 @@ source /home/owrisberg/miniconda3/etc/profile.d/conda.sh
 conda activate base
 
 #Going to folder with data
-cd /home/owrisberg/Coryphoideae/work_flow/3_hybpiper
+cd /home/owrisberg/Coryphoideae/work_flow/03_hybpiper
 
 # Running Intronerate
 while read name; do python3 /home/owrisberg/Coryphoideae/github_code/HybPiper/intronerate.py --prefix $name &>> intronerate_out.txt; done < namelist.txt
