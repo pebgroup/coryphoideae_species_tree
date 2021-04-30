04bff29addaf40360e0b2d34b958e9f29dd64bb5
Oscar <owkien@gmail.com>
2021-04-30 12:23:48 +0200

    Updated coverage_batch to look in right directory.

--- Slurm-scripts/coverage_batch.sh
+++ Slurm-scripts/coverage_batch.sh
@@ -16,7 +16,7 @@ source /home/owrisberg/miniconda3/etc/profile.d/conda.sh
 conda activate base
 
 #Going to folder with data
-cd /home/owrisberg/Coryphoideae/work_flow/3_hybpiper
+cd /home/owrisberg/Coryphoideae/work_flow/03_hybpiper
 
 # Running Wolfs Coverage tester
 while read name; do python3 /home/owrisberg/Coryphoideae/github_code/coryphoideae_species_tree/coverage.py $name; done < namelist.txt
