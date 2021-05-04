88b481c276d2f2b9d38af2d2c4f95863f1701938
Oscar <owkien@gmail.com>
2021-05-04 09:24:47 +0200

    Had to update coverage.py to the new file structure.

--- coverage.py
+++ coverage.py
@@ -30,45 +30,45 @@ for locus in loci:
 			#sequences.append(record)
 			sequences[record.id] = record
 			
-with open('/home/owrisberg/Coryphoideae/test_data/5_coverage/'+sample+'.fasta', "w") as outfile:
+with open('/home/owrisberg/Coryphoideae/work_flow/05_coverage/'+sample+'.fasta', "w") as outfile:
  	SeqIO.write(list(sequences.values()), outfile, "fasta")
 
 print(sample+'.fasta generated')
 	
 # BWA index targets
-cmd = 'bwa index /home/owrisberg/Coryphoideae/test_data/5_coverage/'+sample+'.fasta'
+cmd = 'bwa index /home/owrisberg/Coryphoideae/work_flow/05_coverage/'+sample+'.fasta'
 subprocess.call(cmd,shell=True)
 print(sample+'.fasta indexed')
 
 # BWA mem paired reads
-cmd = 'bwa mem /home/owrisberg/Coryphoideae/test_data/5_coverage/'+sample+'.fasta /home/owrisberg/Coryphoideae/test_data/2_trimmed/'+sample+'_1P.fastq /home/owrisberg/Coryphoideae/test_data/2_trimmed/'+sample+'_2P.fastq | samtools view -b -o /home/owrisberg/Coryphoideae/test_data/5_coverage/'+sample+'.bam'
+cmd = 'bwa mem /home/owrisberg/Coryphoideae/work_flow/05_coverage/'+sample+'.fasta /home/owrisberg/Coryphoideae/work_flow/02_trimmed/'+sample+'_1P.fastq /home/owrisberg/Coryphoideae/work_flow/02_trimmed/'+sample+'_2P.fastq | samtools view -b -o /home/owrisberg/Coryphoideae/work_flow/05_coverage/'+sample+'.bam'
 subprocess.call(cmd,shell=True)
 print('paired reads mapped to '+sample+'.fasta')
 
 # BWA mem unpaired reads
-cmd = 'bwa mem /home/owrisberg/Coryphoideae/test_data/5_coverage/'+sample+'.fasta /home/owrisberg/Coryphoideae/test_data/2_trimmed/'+sample+'_UN.fastq | samtools view -b -o /home/owrisberg/Coryphoideae/test_data/5_coverage/'+sample+'_up.bam'
+cmd = 'bwa mem /home/owrisberg/Coryphoideae/work_flow/05_coverage/'+sample+'.fasta /home/owrisberg/Coryphoideae/work_flow/02_trimmed/'+sample+'_UN.fastq | samtools view -b -o /home/owrisberg/Coryphoideae/work_flow/05_coverage/'+sample+'_up.bam'
 subprocess.call(cmd,shell=True)
 print('unpaired reads mapped to '+sample+'.fasta')
 
 # merge BAM files
-cmd = 'samtools merge /home/owrisberg/Coryphoideae/test_data/5_coverage/'+sample+'_all.bam /home/owrisberg/Coryphoideae/test_data/5_coverage/'+sample+'.bam /home/owrisberg/Coryphoideae/test_data/5_coverage/'+sample+'_up.bam'
+cmd = 'samtools merge /home/owrisberg/Coryphoideae/work_flow/05_coverage/'+sample+'_all.bam /home/owrisberg/Coryphoideae/work_flow/05_coverage/'+sample+'.bam /home/owrisberg/Coryphoideae/work_flow/05_coverage/'+sample+'_up.bam'
 subprocess.call(cmd,shell=True)
 print('BAMs merged')
 
 # sort and index BAM files
-cmd = 'samtools sort /home/owrisberg/Coryphoideae/test_data/5_coverage/'+sample+'_all.bam -o /home/owrisberg/Coryphoideae/test_data/5_coverage/'+sample+'_all_sorted.bam'
+cmd = 'samtools sort /home/owrisberg/Coryphoideae/work_flow/05_coverage/'+sample+'_all.bam -o /home/owrisberg/Coryphoideae/work_flow/05_coverage/'+sample+'_all_sorted.bam'
 subprocess.call(cmd,shell=True)
-cmd = 'samtools index /home/owrisberg/Coryphoideae/test_data/5_coverage/'+sample+'_all_sorted.bam'
+cmd = 'samtools index /home/owrisberg/Coryphoideae/work_flow/05_coverage/'+sample+'_all_sorted.bam'
 subprocess.call(cmd,shell=True)
 print('BAM indexed and sorted')
 
 # remove duplicates
-cmd = 'java -jar ~/software/picard.jar MarkDuplicates I=/home/owrisberg/Coryphoideae/test_data/5_coverage/'+sample+'_all_sorted.bam O=/home/owrisberg/Coryphoideae/test_data/5_coverage/'+sample+'_all_sorted_deduplicated.bam M=/home/owrisberg/Coryphoideae/test_data/5_coverage/'+sample+'marked_dup_metrics.txt REMOVE_DUPLICATES=true'
+cmd = 'java -jar ~/software/picard.jar MarkDuplicates I=/home/owrisberg/Coryphoideae/work_flow/05_coverage/'+sample+'_all_sorted.bam O=/home/owrisberg/Coryphoideae/work_flow/05_coverage/'+sample+'_all_sorted_deduplicated.bam M=/home/owrisberg/Coryphoideae/work_flow/05_coverage/'+sample+'marked_dup_metrics.txt REMOVE_DUPLICATES=true'
 subprocess.call(cmd,shell=True)
 print('reads deduplicated for sample '+sample)
 
 # calculate coverage
-cmd = 'samtools depth /home/owrisberg/Coryphoideae/test_data/5_coverage/'+sample+'_all_sorted_deduplicated.bam > /home/owrisberg/Coryphoideae/test_data/5_coverage/'+sample+'.cov'
+cmd = 'samtools depth /home/owrisberg/Coryphoideae/work_flow/05_coverage/'+sample+'_all_sorted_deduplicated.bam > /home/owrisberg/Coryphoideae/work_flow/05_coverage/'+sample+'.cov'
 subprocess.call(cmd,shell=True)
 print('coverage calculated for sample '+sample)
 
@@ -79,7 +79,7 @@ def n2N(sqnc, pstn):
 	return "".join(sqnc)
 
 # process coverage
-with open('/home/owrisberg/Coryphoideae/test_data/5_coverage/'+sample+'.cov', "r") as covfile:
+with open('/home/owrisberg/Coryphoideae/work_flow/05_coverage/'+sample+'.cov', "r") as covfile:
 	for line in covfile:
 		line = line.strip()
 		LINE = line.split("\t")
@@ -95,7 +95,7 @@ for nm in sequences.keys():
 print('coverage trimming completed, keeping only positions with coverage of '+str(trshld)+' or above')
 
 # write outfile
-with open('/home/owrisberg/Coryphoideae/test_data/5_coverage/'+sample+'_trimmed.fasta', "w") as outfile:
+with open('/home/owrisberg/Coryphoideae/work_flow/05_coverage/'+sample+'_trimmed.fasta', "w") as outfile:
 	SeqIO.write(list(sequences.values()), outfile, "fasta")
 print('trimmed seqs written to '+sample+'_trimmed.fasta')
 
