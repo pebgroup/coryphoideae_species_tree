9aae04ee57c33b44a5a9ee1e893c18f5826b2d4b
Oscar <owkien@gmail.com>
2021-05-12 11:16:31 +0200

    test

--- Slurm-scripts/mafft_batch.sh
+++ Slurm-scripts/mafft_batch.sh
@@ -20,10 +20,10 @@ cd /home/owrisberg/Coryphoideae/work_flow/06_blacklisting
 
 ## This script runs mafft on the fasta files contained within the directory from which it is executed. Remember to adjust number of threads. 
 ## From the manual: "*L-INS-i (probably most accurate; recommended for <200 sequences; iterative refinement method incorporating local pairwise alignment information): mafft --localpair --maxiterate 1000 input [> output] linsi input [> output]". These settings are recommended by Matt Johnson in the KewHybSeqWorkshop.
-echo
-	for f in *; do 
-		linsi --thread 64 $f > /home/owrisberg/Coryphoideae/work_flow/07_alignments/${f/.FNA}_aligned.fasta;
-	one
+
+for f in *; do 
+	echo linsi --thread 64 $f > /home/owrisberg/Coryphoideae/work_flow/07_alignments/${f/.FNA}_aligned.fasta;
+done
 
 ## Wolf's repo:
 # `for f in *; do (linsi --thread 16 $f > ../5_alignments/${f/.FNA}_aligned.fasta); done`
