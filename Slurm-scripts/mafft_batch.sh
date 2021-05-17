f534afe9446bb3ec31228122ce0b463e38febf6c
Oscar <owkien@gmail.com>
2021-05-17 11:02:56 +0200

    Attempting parallel alignment.

--- Slurm-scripts/mafft_batch.sh
+++ Slurm-scripts/mafft_batch.sh
@@ -6,7 +6,7 @@
 #SBATCH --mem-per-cpu=20g
 #SBATCH --cpus-per-task=24
 #              D-HH:MM:SS 
-#SBATCH --time=3-00:00:00
+#SBATCH --time=2-00:00:00
 
 #This line enables the activation of specific conda environments
 source /home/owrisberg/miniconda3/etc/profile.d/conda.sh
@@ -21,9 +21,9 @@ cd /home/owrisberg/Coryphoideae/work_flow/06_blacklisting
 ## This script runs mafft on the fasta files contained within the directory from which it is executed. Remember to adjust number of threads. 
 ## From the manual: "*L-INS-i (probably most accurate; recommended for <200 sequences; iterative refinement method incorporating local pairwise alignment information): mafft --localpair --maxiterate 1000 input [> output] linsi input [> output]". These settings are recommended by Matt Johnson in the KewHybSeqWorkshop.
 
-for f in *; do 
-	linsi --thread 64 $f > /home/owrisberg/Coryphoideae/work_flow/07_alignment/${f}_aligned.fasta;
-done
+#for f in *; do 
+#	linsi --thread 64 $f > /home/owrisberg/Coryphoideae/work_flow/07_alignment/${f}_aligned.fasta;
+#done
 
 ## Wolf's repo:
 # `for f in *; do (linsi --thread 16 $f > ../5_alignments/${f/.FNA}_aligned.fasta); done`
@@ -36,3 +36,9 @@ done
 #for f in *; do 
 #   linsi --thread 64 $f > /home/owrisberg/Coryphoideae/work_flow/07_alignments/${f}.FNA_aligned.fasta;
 #done
+
+
+# Pirada's method of doing mafft
+for f in *.FNA; do (echo ${f/.FNA} >> ./genenames.txt); done # making a list of the genes
+
+parallel --eta "mafft --localpair --adjustdirectionaccurately --maxiterate 1000 {}.FNA > /home/owrisberg/Coryphoideae/work_flow/07_alignment/${f}_aligned.fasta" :::: genenames.txt
