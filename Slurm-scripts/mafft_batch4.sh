545cd54a4676ee6f24861caa01d62977b5676166
Oscar <owkien@gmail.com>
2021-05-18 15:29:58 +0200

    Made horrendous parallelisation of MAFFT.

--- Slurm-scripts/mafft_batch4.sh
+++ Slurm-scripts/mafft_batch4.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #SBATCH --account Coryphoideae
-#SBATCH --job-name=Alignment_mafft
+#SBATCH --job-name=Alignment_mafft_4
 #SBATCH --partition normal
 #SBATCH --mem-per-cpu=20g
 #SBATCH --cpus-per-task=24
 #              D-HH:MM:SS 
-#SBATCH --time=2-00:00:00
+#SBATCH --time=1-00:00:00
 
 #This line enables the activation of specific conda environments
 source /home/owrisberg/miniconda3/etc/profile.d/conda.sh
@@ -21,10 +21,6 @@ cd /home/owrisberg/Coryphoideae/work_flow/06_blacklisting
 ## This script runs mafft on the fasta files contained within the directory from which it is executed. Remember to adjust number of threads. 
 ## From the manual: "*L-INS-i (probably most accurate; recommended for <200 sequences; iterative refinement method incorporating local pairwise alignment information): mafft --localpair --maxiterate 1000 input [> output] linsi input [> output]". These settings are recommended by Matt Johnson in the KewHybSeqWorkshop.
 
-# Pirada's method of doing mafft
-#for f in *.FNA; do (echo ${f/.FNA} >> ./genenames.txt); done # making a list of the genes
-
-
-for f in genelist.txt; do 
+for f in genenames4.txt; do 
 	linsi --adjustdirectionaccurately --thread 64 $f > /home/owrisberg/Coryphoideae/work_flow/07_alignment/${f}_aligned.fasta;
 done
