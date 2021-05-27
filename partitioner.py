d7696f4d99f79d7c8b4432171b9b845e792ff559
Oscar <owkien@gmail.com>
2021-05-27 13:56:37 +0200

    Attempt with len(sequences) != len(tally)

--- partitioner.py
+++ partitioner.py
@@ -16,9 +16,9 @@ for fn in os.listdir():
 		print(fn)
 		sequences = [] # gather sequences to keep in the final alignment (all but the exons)
 		# extract aligned exon sequences
+		tally=[]
 		for record in SeqIO.parse(fn, "fasta"):
-			exon1=None # Defining exon 1 and 2 so the next for loop doesnt throw an error when exon1 doesnt exists
-			exon2=None
+			tally.append(record)
 			if record.id == "exon1":
 				exon1 = record.seq
 				print(record,"is exon1")
@@ -33,8 +33,8 @@ for fn in os.listdir():
 		binpart = []
 
 
-		if exon1 in locals():
-			for i in range(len(exon1)):
+		if len(sequences) != len(tally):
+			for i in range(len(exon2)):
 				# assumes that any alignment pos. where ANY of the two exons has a base is exon
 				if  len(exon1) == len(exon2):
 					if exon1[i] == "-" and exon2[i] == "-":
@@ -44,33 +44,33 @@ for fn in os.listdir():
 				else:
 						print("Error", fn ,len(exon1),len(exon2))
 
-		#print(binpart)
-		# define partitions as ranges
-		partitions_intron = []
-		partitions_exon = []
-		current_part = [1, "NA"]
-		current_state = binpart[0]
-		for i in range(len(binpart)):
-			if binpart[i] != current_state: # when a shift occurs...
-				# define search window for smoothing over mini-partitions
-				if (i+smoother) < len(binpart): # to avoid index errors
-					wndw = binpart[i:(i+smoother)]
-				else:
-					wndw = binpart[i:]
-				if len(set(wndw)) == 1: # only invoke a partition shift if there is no further partition shift within n=smoother basepairs upstream
-					current_part[1] = i # set end position of current partition
+			#print(binpart) 
+			# define partitions as ranges
+			partitions_intron = []
+			partitions_exon = []
+			current_part = [1, "NA"]
+			current_state = binpart[0]
+			for i in range(len(binpart)):
+				if binpart[i] != current_state: # when a shift occurs...
+					# define search window for smoothing over mini-partitions
+					if (i+smoother) < len(binpart): # to avoid index errors
+						wndw = binpart[i:(i+smoother)]
+					else:
+						wndw = binpart[i:]
+					if len(set(wndw)) == 1: # only invoke a partition shift if there is no further partition shift within n=smoother basepairs upstream
+						current_part[1] = i # set end position of current partition
+						if current_state == 0:  # append concluded partition to partitions_intron or partitions_exon, depending on current_state
+							partitions_intron.append(current_part)
+						else:
+							partitions_exon.append(current_part)
+						current_part = [i+1, "NA"] # initialise new current position
+						current_state = binpart[i] # initialise new current state
+				if i == len(binpart)-1: # conclude and append last partition
+					current_part[1] = i+1
 					if current_state == 0:  # append concluded partition to partitions_intron or partitions_exon, depending on current_state
 						partitions_intron.append(current_part)
 					else:
 						partitions_exon.append(current_part)
-					current_part = [i+1, "NA"] # initialise new current position
-					current_state = binpart[i] # initialise new current state
-			if i == len(binpart)-1: # conclude and append last partition
-				current_part[1] = i+1
-				if current_state == 0:  # append concluded partition to partitions_intron or partitions_exon, depending on current_state
-					partitions_intron.append(current_part)
-				else:
-					partitions_exon.append(current_part)
 		# write RAxML style partition file
 		with open(".".join(fn.split(".")[:-1])+"_part.txt", "w") as partfile:
 			print("DNA, intron = " + ", ".join(["-".join([str(j) for j in i]) for i in partitions_intron]), file=partfile)
