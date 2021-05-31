e5286751c2b1fe19ff55a3fa5df5a816434023d2
Oscar <owkien@gmail.com>
2021-05-31 11:11:46 +0200

    Added parallelisation for treebuilder_batch.sh

--- infrastructure.sh
+++ infrastructure.sh
@@ -2,13 +2,12 @@
 
 #This script is meant to produce the necessary file structure for the workflow within a directory
 
+#Building main folders
 mkdir 00_secapr
-mkdir 00_secapr/0_data
-mkdir 00_secapr/1_trimmed
 mkdir 01_data
 mkdir 02_trimmed
 mkdir 03_hybpiper
-mkdir 04_seqs
+mkdir 04_seqs # This folder is empty?
 mkdir 05_coverage
 mkdir 06_blacklisting
 mkdir 07_alignment
@@ -16,3 +15,13 @@ mkdir 08_mapping
 mkdir 09_optrimal
 mkdir 10_manual-edit
 mkdir 11_tree_building
+
+#Building sub folders
+#00
+mkdir 00_secapr/0_data
+mkdir 00_secapr/1_trimmed
+
+mkdir10_manual_edit/01_alignments_for_editing
+mkdir10_manual_edit/02_edited_alignments
+mkdir10_manual_edit/03_bad_alignments
+mkdir10_manual_edit/04_alignments_for_trees
