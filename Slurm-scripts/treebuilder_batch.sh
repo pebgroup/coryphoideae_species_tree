3ff9180764ad031b52e9dc8e4bf93139bd6f5cc4
Oscar <owkien@gmail.com>
2021-06-01 18:32:56 +0200

    testing rerooting.

--- Slurm-scripts/treebuilder_batch.sh
+++ Slurm-scripts/treebuilder_batch.sh
@@ -15,44 +15,44 @@ source /home/owrisberg/miniconda3/etc/profile.d/conda.sh
 conda activate treebuilder_env
 
 #Cleaning folder with data
-cd /home/owrisberg/Coryphoideae/work_flow/10_manual-edit/04_alignments_for_trees
-rm *
+#cd /home/owrisberg/Coryphoideae/work_flow/10_manual-edit/04_alignments_for_trees
+#rm *
 
 #Copying data from manual alignment folder
-cd /home/owrisberg/Coryphoideae/work_flow/10_manual-edit/02_edited_alignments
-cp *fasta ../04_alignments_for_trees
+#cd /home/owrisberg/Coryphoideae/work_flow/10_manual-edit/02_edited_alignments
+#cp *fasta ../04_alignments_for_trees
 
 #Going to folder with data
-cd /home/owrisberg/Coryphoideae/work_flow/10_manual-edit/04_alignments_for_trees
+#cd /home/owrisberg/Coryphoideae/work_flow/10_manual-edit/04_alignments_for_trees
 
 #The partitioner should produce 2 files for each gene
 #one file called {gene}_aligned_part.txt which is the partitioning file
 #another called {gene}_aligned_clean.fasta which are just the sequences without the exons
-python3 /home/owrisberg/Coryphoideae/github_code/coryphoideae_species_tree/partitioner.py --smoother 10 
+#python3 /home/owrisberg/Coryphoideae/github_code/coryphoideae_species_tree/partitioner.py --smoother 10 
 
 
-echo "Beginning IQtree genetree search"
+# echo "Beginning IQtree genetree search"
 
-for f in *_part.txt; do (cp $f ${f/_part.txt}_clean.part); done
-ls *clean.fasta | parallel -j 6 iqtree2 -s {} -T AUTO -ntmax 4 -p {.}.part -B 1000
+# for f in *_part.txt; do (cp $f ${f/_part.txt}_clean.part); done
+# ls *clean.fasta | parallel -j 6 iqtree2 -s {} -T AUTO -ntmax 4 -p {.}.part -B 1000
 
-echo "Done with IQtree"
+# echo "Done with IQtree"
 
-echo "Beginning cleanup and file transfer"
+# echo "Beginning cleanup and file transfer"
 
-for f in *_aligned_clean.fasta
-do
-	mv ${f/clean.fasta}clean_part.treefile /home/owrisberg/Coryphoideae/work_flow/11_tree_building/01_genetrees/${f/clean.fasta}part.txt.tre
-	mv ${f/clean.fasta}part.txt* /home/owrisberg/Coryphoideae/work_flow/11_tree_building/01_genetrees
-	mv ${f/_aligned_clean.fasta}.fasta done
-	#rm ${f}
-done
+# for f in *_aligned_clean.fasta 
+# do
+# 	mv ${f/clean.fasta}clean.part.treefile /home/owrisberg/Coryphoideae/work_flow/11_tree_building/01_genetrees/${f/clean.fasta}part.txt.tre
+# 	mv ${f/clean.fasta}part.txt* /home/owrisberg/Coryphoideae/work_flow/11_tree_building/01_genetrees #This works
+# 	mv ${f/_aligned_clean.fasta}.fasta 05_alignments_used_in_trees
+# 	rm ${f}
+# done
 
 
 cd /home/owrisberg/Coryphoideae/work_flow/11_tree_building/01_genetrees
-rm -f /home/owrisberg/Coryphoideae/work_flow/11_tree_building/02_speciestree
+# rm -f /home/owrisberg/Coryphoideae/work_flow/11_tree_building/02_speciestree
 
-echo "Done with cleanup"
+# echo "Done with cleanup"
 echo "Starting rerooting of genetrees"
 
 #Some rerooting, renaming and general cleanup
