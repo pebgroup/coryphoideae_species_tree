e68d481232004983274167e081957277787156c2
Oscar <owkien@gmail.com>
2021-06-02 13:51:32 +0200

    added strip and replace

--- genenameremover.py
+++ genenameremover.py
@@ -13,10 +13,13 @@ treefile = str(args.treefile)
 # Go through each tip of the gene tree
 tree = dendropy.Tree.get(path=treefile, schema="newick")
 tips = str(tree.taxon_namespace)
+tips = tips.strip("[")
+tips = tips.strip("]")
+tips = tips.replace("'","")
 tipslist = tips.split(", ")
 
 print("tips",tips,"\n")
 print("tipslist",tipslist,"\n")
 
-#for j in range(len(tips)):
-#   print(tips[j],j)
+for j in range(len(tipslist)):
+   print(tipslist[j],j)
