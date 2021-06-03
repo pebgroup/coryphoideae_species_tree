8a582ec6a0dbc846215a328a0b849f8f060e39f6
Oscar <owkien@gmail.com>
2021-06-03 09:22:52 +0200

    Edited ; to ,

--- renamer.py
+++ renamer.py
@@ -2,21 +2,24 @@
 
 import argparse, re
 
+#Arguments from commandline
 parser = argparse.ArgumentParser()
 parser.add_argument("mapping")
 parser.add_argument("infile")
 parser.add_argument("outfile")
 parser.add_argument('--bs', default=0, help='1 = tree with bootstrap values')
 
+#parsing args
 args = parser.parse_args()
 
+#Reading tips from tree
 with open(args.infile) as f:
 	tree = f.readline().strip()
 	
 with open(args.mapping, mode='r', encoding='utf-8-sig') as f: 
 	for line in f:
 		line = line.strip()
-		LINE = line.split(";")
+		LINE = line.split(",")
 		searchterm = LINE[0]
 		if args.bs == 0:
 			tree = re.sub(rf'\({searchterm},','('+LINE[1]+',',tree)
