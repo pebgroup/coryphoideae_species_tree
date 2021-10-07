c88483f2f62ee501f85dc69c8875674cf3433c3c
Oscar <owkien@gmail.com>
2021-10-07 14:01:19 +0200

    updated time running time for trimming.

---
+++ Slurm-scripts/secapr_batch_namelist_data.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#SBATCH --account Coryphoideae
+#SBATCH --job-name=Secapr_data
+#SBATCH --partition normal
+#SBATCH --mem-per-cpu=30g
+#SBATCH --cpus-per-task=10
+#              D-HH:MM:SS
+#SBATCH --time=4-00:00:00
+
+
+#This line should enable the activation of specific conda environments
+source /home/owrisberg/miniconda3/etc/profile.d/conda.sh
+
+#Activating conda base environment 
+conda activate secapr_env
+
+#Going to folder with data
+cd /home/owrisberg/Coryphoideae/work_flow/01_data
+
+# Running secapr
+
+
+#Moving the results to the correct folder
+mv *.zip ../00_secapr/0_data
+mv *.html ../00_secapr/0_data
+mv *.txt ../00_secapr/0_data
+mv *.pdf ../00_secapr/0_data
+
+
+
