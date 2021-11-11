9df0bc02169492e3dfbe7af72cd65e16aa63fcc2
Oscar <owkien@gmail.com>
2021-11-11 11:20:23 +0100

    Edited outputs

--- workflow.py
+++ workflow.py
@@ -67,7 +67,7 @@ gwf = Workflow()
 def hybpiper(species, p1, p2, un, path_out, path_in, done):
     """Hybpiper."""
     inputs = [path_in + p1, path_in + p2, path_in + un, species]
-    outputs = [path_out , path_out + done]
+    outputs = [path_out + done]
     options = {'cores': 1, 'memory': "20g", 'walltime': "8:00:00", 'account':"Coryphoideae"}
 
     spec = """
