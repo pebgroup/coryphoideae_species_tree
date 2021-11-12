7891ee8aeb48b7b777ba402e04c092edf65e36ee
Oscar <owkien@gmail.com>
2021-11-12 12:54:47 +0100

    edited the path_out string

--- workflow.py
+++ workflow.py
@@ -67,7 +67,7 @@ gwf = Workflow()
 def hybpiper(species, p1, p2, un, path_out, path_in, done):
     """Hybpiper."""
     inputs = [path_in + species +p1, path_in + species + p2, path_in + species + un]
-    outputs = [path_out+ "/" + species, done]
+    outputs = [path_out + "/" + species, done]
     options = {'cores': 1, 'memory': "20g", 'walltime': "8:00:00", 'account':"Coryphoideae"}
 
     spec = """
@@ -157,7 +157,7 @@ for i in range(len(sp)):
                                                         un = "_UN.fastq",
                                                         path_out= "/home/owrisberg/Coryphoideae/work_flow/03_hybpiper/",
                                                         path_in = "/home/owrisberg/Coryphoideae/work_flow/02_trimmed/",
-                                                        done = "/home/owrisberg/Coryphoideae/work_flow/03_hybpiper/done/"))
+                                                        done = "/home/owrisberg/Coryphoideae/work_flow/03_hybpiper/done"))
                                                                       
     
 
