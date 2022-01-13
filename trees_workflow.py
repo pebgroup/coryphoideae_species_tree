4d7d947cef5cf1bd749fdd18d35d375292ef2fd7
Oscar <owkien@gmail.com>
2022-01-13 11:08:44 +0100

    fixed indentation error (I hope)

--- trees_workflow.py
+++ trees_workflow.py
@@ -111,6 +111,6 @@ for i in range(len(genes)):
                                                         path_in = "/home/owrisberg/Coryphoideae/work_flow/09_manual_edit/04_alignments_for_trees/"))
 
 	#Running IQ_tree
-	gwf.target_from_template('IQtree_'+genes[i], iq_tree(gene = genes[i],
-	                                                	path_in = "/home/owrisberg/Coryphoideae/work_flow/09_manual_edit/04_alignments_for_trees/",
-														path_out = "/home/owrisberg/Coryphoideae/work_flow/10_tree_building/01_genetrees/"))
+    gwf.target_from_template('IQtree_'+genes[i], iq_tree(gene = genes[i],
+                                                        path_in = "/home/owrisberg/Coryphoideae/work_flow/09_manual_edit/04_alignments_for_trees/",
+                                                        path_out = "/home/owrisberg/Coryphoideae/work_flow/10_tree_building/01_genetrees/"))
