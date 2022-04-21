26a3fdbbd21b1aab411d6aaca78f5f1ec72fcd3c
Oscar <owkien@gmail.com>
2022-04-21 11:09:21 +0200

    updated paths

--- Slurm-scripts/Sorta_date.sh
+++ Slurm-scripts/Sorta_date.sh
@@ -19,17 +19,17 @@ source /home/owrisberg/miniconda3/etc/profile.d/conda.sh
 conda activate base
 
 #Get the root-to-tip variance with
-python /home/owrisberg/Coryphoideae/github_code/SortaDate/src/get_var_length.py /home/owrisberg/Coryphoideae/work_flow/10_tree_building/01_genetrees/ --flend _part.txt.tre --outf /home/owrisberg/Coryphoideae/work_flow/11_dating_the_tree/00_sortadate/var --outg 1079,1080,1081,1082
+python /home/owrisberg/Coryphoideae/github_code/SortaDate/src/get_var_length.py /home/owrisberg/Coryphoideae/work_flow/10_tree_building/01_genetrees/ --flend _part.txt.tre --outf /home/owrisberg/Coryphoideae/work_flow/11_dating_the_tree/00_sortadate/var/ --outg 1079,1080,1081,1082
 
 #Get the bipartition support with
 #What kind of values is the values shown in the example file Chrono_Tent_Bird_study.new, and do i need to care?
-python /home/owrisberg/Coryphoideae/github_code/SortaDate/src/get_bp_genetrees.py /home/owrisberg/Coryphoideae/work_flow/10_tree_building/01_genetrees/ /home/owrisberg/Coryphoideae/work_flow/10_tree_building/02_speciestree/astral_treee.tre --flend _part.txt.tre --outf /home/owrisberg/Coryphoideae/work_flow/11_dating_the_tree/00_sortadate/bp
+python /home/owrisberg/Coryphoideae/github_code/SortaDate/src/get_bp_genetrees.py /home/owrisberg/Coryphoideae/work_flow/10_tree_building/01_genetrees/ /home/owrisberg/Coryphoideae/work_flow/10_tree_building/02_speciestree/astral_treee.tre --flend _part.txt.tre --outf /home/owrisberg/Coryphoideae/work_flow/11_dating_the_tree/00_sortadate/bp/
 
 #Combine the results from these two runs with
-python /home/owrisberg/Coryphoideae/github_code/SortaDate/src/combine_results.py /home/owrisberg/Coryphoideae/work_flow/11_dating_the_tree/00_sortadate/var /home/owrisberg/Coryphoideae/work_flow/11_dating_the_tree/00_sortadate/bp --outf /home/owrisberg/Coryphoideae/work_flow/11_dating_the_tree/00_sortadate/comb
+python /home/owrisberg/Coryphoideae/github_code/SortaDate/src/combine_results.py /home/owrisberg/Coryphoideae/work_flow/11_dating_the_tree/00_sortadate/var /home/owrisberg/Coryphoideae/work_flow/11_dating_the_tree/00_sortadate/bp --outf /home/owrisberg/Coryphoideae/work_flow/11_dating_the_tree/00_sortadate/comb/
 
 #Sort and get the list of the good genes with
-python /home/owrisberg/Coryphoideae/github_code/SortaDate/src/get_good_genes.py /home/owrisberg/Coryphoideae/work_flow/11_dating_the_tree/00_sortadate/comb --max 3 --order 3,1,2 --outf /home/owrisberg/Coryphoideae/work_flow/11_dating_the_tree/00_sortadate/gg
+python /home/owrisberg/Coryphoideae/github_code/SortaDate/src/get_good_genes.py /home/owrisberg/Coryphoideae/work_flow/11_dating_the_tree/00_sortadate/comb --max 3 --order 3,1,2 --outf /home/owrisberg/Coryphoideae/work_flow/11_dating_the_tree/00_sortadate/gg/
 
 
 
