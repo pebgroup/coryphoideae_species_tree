a13a54c9b496744fff02287c86578d5f9ceaac0c
Oscar <owkien@gmail.com>
2022-06-15 14:50:36 +0200

    added additional output to trimmomatic

--- species_workflow.py
+++ species_workflow.py
@@ -61,7 +61,7 @@ def trimmomatic(species, path_in, path_out, done):
     Afterwards combines paired and unpaired reads for forward and reverse reads respectively for each species 
     to enable post-trimming secapr quality_check for comparability before and after trimming """
     inputs = []
-    outputs = [path_out+species+".fastq", done]
+    outputs = [path_out+species+".fastq", done, path_out+species+"_1P.fastq", path_out+species+"_2P.fastq", path_out+species+"_UN.fastq"]
     options = {'cores': 16, 'memory': "10g", 'walltime': "00:30:00", 'account':"Coryphoideae"}
 
     spec = """
