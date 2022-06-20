7dbd4244b42121eb35d386e101b41a758ea73887
Oscar <owkien@gmail.com>
2022-06-20 10:34:50 +0200

    removed dir_wrk

--- species_workflow.py
+++ species_workflow.py
@@ -207,7 +207,7 @@ def intronerate(species, path_in, done):
 # ########################################################################################################################
 # #############################################---- Coverage ----#########################################################
 # ########################################################################################################################
-def coverage(species, path_in, path_out, done,all_bam,all_sorted_bam, all_sorted_bam_bai, bam, cov,fasta,fasta_amb,fasta_ann,fasta_bwt,fasta_pac,fasta_sa,trimmed_fasta,up_bam,dir_in,dir_out,dir_wrk):
+def coverage(species, path_in, path_out, done,all_bam,all_sorted_bam, all_sorted_bam_bai, bam, cov,fasta,fasta_amb,fasta_ann,fasta_bwt,fasta_pac,fasta_sa,trimmed_fasta,up_bam,dir_in,dir_out):
     """Calculating coverage of sequences."""
     inputs = [path_in+species, path_in+"done/Intronerate/"+species]
     outputs = [path_out+species+all_bam, path_out+species+all_sorted_bam, path_out+species+all_sorted_bam_bai, path_out+species+bam,
