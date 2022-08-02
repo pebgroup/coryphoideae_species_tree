1f6fd13af596c89502d8931327ca89120883b826
Oscar <owkien@gmail.com>
2022-08-02 09:26:01 +0200

    removed 1PU and 2PU from outputs of fastqc_trimmed

--- species_workflow.py
+++ species_workflow.py
@@ -54,7 +54,7 @@ def fastqc_raw(species,path_in ,path_out, done,):
 def fastqc_trimmed(species,path_in ,path_out, done,):
     """Quality checking using fastqc as this should work on individual species"""
     inputs = [path_in+species+"_UN.fastq", path_in+species+"_1P.fastq", path_in+species+"_2P.fastq","/home/owrisberg/Coryphoideae/work_flow/02_trimmed/done/"+species]
-    outputs = [path_out+species+"_1P_fastqc.html", path_out+species+"_2P_fastqc.html",path_out+species+"_1PU_fastqc.html",path_out+species+"_2PU_fastqc.html",path_out+species+"_UN_fastqc.html" ,done]
+    outputs = [path_out+species+"_1P_fastqc.html", path_out+species+"_2P_fastqc.html",path_out+species+"_UN_fastqc.html" ,done]
     options = {'cores': 1, 'memory': "10g", 'walltime': "00:30:00", 'account':"Coryphoideae"}
 
 
