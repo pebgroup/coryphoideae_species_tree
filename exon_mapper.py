c9229b40604b65ef4e18b32648b958dff8810553
Oscar <owkien@gmail.com>
2022-09-12 13:20:54 +0200

    edited paths in exon mapper

--- exon_mapper.py
+++ exon_mapper.py
@@ -25,11 +25,11 @@ if gene in df.columns:
 	df_red = df_red.reset_index()
 	# first sample
 	sp = df_red["Species"][0]
-	exon1 = list(SeqIO.parse("../03_hybpiper/"+sp+"/"+gene+"/"+sp+"/sequences/FNA/"+gene+".FNA", "fasta"))[0]
+	exon1 = list(SeqIO.parse("/home/owrisberg/Coryphoideae/work_flow/03_hybpiper/"+sp+"/"+gene+"/"+sp+"/sequences/FNA/"+gene+".FNA", "fasta"))[0]
 	exon1.id = "exon1"
 	# second sample
 	sp = df_red["Species"][1]
-	exon2 = list(SeqIO.parse("../03_hybpiper/"+sp+"/"+gene+"/"+sp+"/sequences/FNA/"+gene+".FNA", "fasta"))[0]
+	exon2 = list(SeqIO.parse("/home/owrisberg/Coryphoideae/work_flow/03_hybpiper/"+sp+"/"+gene+"/"+sp+"/sequences/FNA/"+gene+".FNA", "fasta"))[0]
 	exon2.id = "exon2"
 	with open(gene+"_temp.fasta", "w") as output_handle:
 		SeqIO.write([exon1, exon2], output_handle, "fasta")
