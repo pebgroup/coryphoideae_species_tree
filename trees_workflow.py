56e70b4c6043ec155e573be7ffc98cd764989ca9
Oscar <owkien@gmail.com>
2022-11-24 13:51:08 +0100

    Test

--- trees_workflow.py
+++ trees_workflow.py
@@ -202,7 +202,7 @@ def astral(path_in, gene_tree_file,output):
     """Using Astral to construct a species tree based on the genetrees"""
     inputs = [path_in+gene_tree_file]
     outputs = [path_in+output]
-    options = {'cores': 20, 'memory': "40g", 'walltime': "24:00:00", 'account':"Coryphoideae"}
+    options = {'cores': 20, 'memory': "40g", 'walltime': "1:00:00", 'account':"Coryphoideae"}
 
     spec = """
 
