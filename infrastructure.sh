ef05c66668e0c7bb0b38af8008f4d9f4a9399489
Oscar <owkien@gmail.com>
2023-01-02 12:43:14 +0100

    Added environments to the infrastructure script.

--- infrastructure.sh
+++ infrastructure.sh
@@ -3,32 +3,32 @@
 #This script is meant to produce the necessary file structure for the workflow within a directory
 
 #Building main folders
-echo mkdir 00_secapr_TEST
-echo mkdir 01_data_TEST
-echo mkdir 02_trimmed_TEST
-echo mkdir 03_hybpiper_TEST
-echo mkdir 04_coverage_TEST
-echo mkdir 05_blacklisting_TEST
-echo mkdir 06_alignment_TEST
-echo mkdir 07_mapping_TEST
-echo mkdir 08_optrimal_TEST
-echo mkdir 09_manual-edit_TEST
-echo mkdir 10_tree_building_TEST
+mkdir 00_secapr_TEST
+mkdir 01_data_TEST
+mkdir 02_trimmed_TEST
+mkdir 03_hybpiper_TEST
+mkdir 04_coverage_TEST
+mkdir 05_blacklisting_TEST
+mkdir 06_alignment_TEST
+mkdir 07_mapping_TEST
+mkdir 08_optrimal_TEST
+mkdir 09_manual-edit_TEST
+mkdir 10_tree_building_TEST
 
 #Building sub folders
 #00
-echo mkdir 00_secapr/0_data_TEST
-echo mkdir 00_secapr/1_trimmed_TEST
+mkdir 00_secapr/0_data_TEST
+mkdir 00_secapr/1_trimmed_TEST
 
-echo mkdir10_manual_edit/01_alignments_for_editing_TEST
-echo mkdir10_manual_edit/02_edited_alignments_TEST
-echo mkdir10_manual_edit/03_bad_alignments_TEST
-echo mkdir10_manual_edit/04_alignments_for_trees_TEST
+mkdir10_manual_edit/01_alignments_for_editing_TEST
+mkdir10_manual_edit/02_edited_alignments_TEST
+mkdir10_manual_edit/03_bad_alignments_TEST
+mkdir10_manual_edit/04_alignments_for_trees_TEST
 
 
 # Creating the required environments based on the files in the environment folder
 for ENV_FILE in ./environments/*
 do
 	name_var="${ENV_FILE#./environments/}"
-	echo conda env create --name "TEST_${name_var%_env.txt}"  --file $ENV_FILE
+	conda env create --name "TEST_${name_var%_env.txt}"  --file $ENV_FILE
 done
