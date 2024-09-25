283676665162ed3d2aade3703d17645602d7af5d
Oscar <owkien@gmail.com>
2024-09-25 11:08:18 +0200

    Added a missing ")"

--- species_workflow.py
+++ species_workflow.py
@@ -375,7 +375,7 @@ for i in range(len(sp)):
                                                         path_out= "/home/owrisberg/Coryphoideae/work_flow/03_hybpiper/",
                                                         path_in = "/home/owrisberg/Coryphoideae/work_flow/02_trimmed/",
                                                         done = "/home/owrisberg/Coryphoideae/work_flow/03_hybpiper/done/Hybpiper/"+sp[i],
-                                                        target_file = os.path.join(workflow_dir,"../target_sequence/PhyloPalms_loci_renamed_794-176_HEYcorrected.fasta"))
+                                                        target_file = os.path.join(workflow_dir,"../target_sequence/PhyloPalms_loci_renamed_794-176_HEYcorrected.fasta")))
 
 
     #### Paralogs
